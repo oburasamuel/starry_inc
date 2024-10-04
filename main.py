4a7e83ae7e92f396c24588921d86cf1af05c279e
Sam <oburasamuel4@gmail.com>
2024-10-04 08:03:11 +0300

    withdrawal function

--- main.py
+++ main.py
@@ -9,3 +9,21 @@ class User:
         self.amount += amount
 
         print(f'{self.username} deposited ${amount}. New balance: ${self.balance}')
+
+    def withdraw(self, amount):
+        if self.balance >= amount:
+            self.balance -= amount
+
+            print(f'{self.username} withdraw ${amount}. New balance: ${self.balance}')
+
+        else:
+            print(f'{self.username} has insufficient funds to withdraw ${amount}. Current balance is {self.balance}')
+
+    def transfer(self, amount, recipient):
+
+        if self.balance >= amount:
+            self.balance -= amount
+
+            recipient.balance += amount
+
+            print(f'{self.username} transferred ${amount} to {recipient.username}.')
